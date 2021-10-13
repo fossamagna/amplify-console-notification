df114ecd891cd67762aa5edd1365f42871087b89
Hayato Watanabe <solideye@me.com>
2021-10-13 18:18:30 +0900

    add Slack attachment color

--- notification/src/message-sender/slack-message-sender.ts
+++ notification/src/message-sender/slack-message-sender.ts
@@ -5,8 +5,21 @@ import axios from "axios";
 export default class SlackMessageSender implements MessageSender {
   sendMessage(record: SNSEventRecord) {
     const slackHookUrl = process.env.WEBHOOK_URL!;
+
+    const text = record.Sns.Message;
+    
+    let color = "#C0C0C0";
+    if (text.includes("SUCCEED")) {
+      color = "good";
+    } else if (text.includes("FAILED")) {
+      color = "danger";
+    } else if (text.includes("STARTED")) {
+      color = "#EBF6F7";
+    }
+
     const body = {
-      text: record.Sns.Message
+      text: text,
+      color: color
     };
     return axios.post(slackHookUrl, body);
   }
